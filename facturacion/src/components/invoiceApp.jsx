4c741fb670c4049b9925e1509c44a1b3de5f9efb
Oscar <oscarcr189@outlook.com>
2024-03-20 21:35:15 -0600

    Boostrap

--- facturacion/src/components/invoiceApp.jsx
+++ facturacion/src/components/invoiceApp.jsx
@@ -3,58 +3,81 @@ import { getInvoice } from "../services/getInvoice"
 
 export const InvoiceApp = () => {
 
-    const {id,name,client,company,items} = getInvoice();
+   const { id, name, client, company, items } = getInvoice();
 
-    const{name:nameCliente, lastName,address}=client;
-    const {country,city,street,number} = address;
-    return(
-        <>
-         <h1>Ejemplo Factura</h1>        
-         <ul>
-            <li>Id: {id}</li>
-            <li>Name: {name}</li>
-
-         </ul>
-
-         <h3>Datos del Cliente</h3>
-         <ul>
-            <li>{nameCliente} {lastName}</li>
-            <li>{country}</li>
-            <li>{city}</li>
-            <li>{street} {number}</li>
-
-         </ul>
+   const { name: nameCliente, lastName, address } = client;
+   const { country, city, street, number } = address;
+   return (
+      <>
 
 
-         <h3>Datos de la Empresa</h3>
-         <ul>
-            <li>{company.name}</li>
-            <li>{company.fiscalNumber}</li>
-         </ul>
+         <div className="container">
 
-         <h4>Productos de la Factura</h4>
-         <table>
-            <thead>
-               <tr>
-               <th>Producto</th>
-               <th>Precio</th>
-               <th>Cantidad</th>
-               </tr>
-            </thead>
-            <tbody>
-               {items.map(({id,product,price,quantity}) => ( <tr key={id}>
+            <div className="card my-3">
 
-                        <td> {product} </td>
-                        <td> {price} </td>
-                        <td> {quantity} </td>
+               <div className="card-header">
+                  Ejemplo Factura
+               </div>
 
-                     </tr>
+               <div className="card-body">
 
-                  ))}
-               
-            </tbody>
-         </table>
-        </>     
-        
-    )
+
+
+                  <ul className="list-group">
+                     <li className="list-group-item" >Id: {id}</li>
+                     <li className="list-group-item" >Name: {name}</li>
+
+                  </ul>
+
+                  <div className="row my-3">
+                     <div className="col">
+
+                        <h3>Datos del Cliente</h3>
+                        <ul className="list-group">
+                           <li className="list-group-item active">{nameCliente} {lastName}</li>
+                           <li className="list-group-item">{country} / {city}</li>
+                           <li className="list-group-item">{street} {number}</li>
+
+                        </ul>
+                     </div>
+
+                     <div className="col">
+                        <h3>Datos de la Empresa</h3>
+                        <ul className="list-group">
+                           <li className="list-group-item active">{company.name}</li>
+                           <li className="list-group-item">{company.fiscalNumber}</li>
+                        </ul>
+                     </div>
+                  </div>
+
+                  <h4>Productos de la Factura</h4>
+
+                  <table className="table table-striped table-hover">
+                     <thead>
+                        <tr>
+                           <th>Producto</th>
+                           <th>Precio</th>
+                           <th>Cantidad</th>
+                        </tr>
+                     </thead>
+                     <tbody>
+                        {items.map(({ id, product, price, quantity }) => (<tr key={id}>
+
+                           <td> {product} </td>
+                           <td> {price} </td>
+                           <td> {quantity} </td>
+
+                        </tr>
+
+                        ))}
+
+                     </tbody>
+                  </table>
+               </div>
+            </div>
+         </div>
+
+      </>
+
+   )
 }
