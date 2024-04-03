04732c4e3f99fe916fbba1a66715433f189a87eb
Oscar <oscarcr189@outlook.com>
2024-04-02 20:45:12 -0600

    optimizacion de estructuracion

--- facturacion/src/components/ClientView.jsx
+++ facturacion/src/components/ClientView.jsx
@@ -1,8 +1,7 @@
 import PropTypes from "prop-types";
 export const ClientView = ({title, client}) => {
 
-    const { name: nameClient, lastName, address } = client;
-    const { country, city, street, number } = address;
+    const { name: nameClient, lastName, address: { country, city, street, number }  } = client;
 
     return (
         <>
